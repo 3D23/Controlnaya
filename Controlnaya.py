a47caff6c9862ba0b17b7b90cfdde5b8bf9e3c09
Dima Dmitrichenkov <114085868+3D23@users.noreply.github.com>
2022-12-12 11:35:34 +0300

    Update Controlnaya.py

--- Controlnaya.py
+++ Controlnaya.py
@@ -6,26 +6,26 @@ class Auto():
         self.direction = direction
 
     def move(self, go):
-        if self.direction == 'Вправо':
+        if self.direction.lower() == 'вправо':
             self.x += abs(go)
-            print('Двигаемся вправо на ' + str(go) + 'единиц')
+            print('Двигаемся вправо на ' + str(go) + ' единиц')
             print('Текущее положение: x - ' + str(self.x) + ' y - ' + str(self.y))
-        elif self.direction == 'Влево':
+        elif self.direction.lower() == 'влево':
             self.x -= abs(go)
-            print('Двигаемся влево на ' + str(go) + 'единиц')
+            print('Двигаемся влево на ' + str(go) + ' единиц')
             print('Текущее положение: x - ' + str(self.x) + ' y - ' + str(self.y))
-        elif self.direction == 'Вверх':
+        elif self.direction.lower() == 'вверх':
             self.y += abs(go)
-            print('Двигаемся вверх на ' + str(go) + 'единиц')
+            print('Двигаемся вверх на ' + str(go) + ' единиц')
             print('Текущее положение: x - ' + str(self.x) + ' y - ' + str(self.y))
-        elif self.direction == 'Вниз':
+        elif self.direction.lower() == 'вниз':
             self.x -= abs(go)
-            print('Двигаемся вниз на ' + str(go) + 'единиц')
+            print('Двигаемся вниз на ' + str(go) + ' единиц')
             print('Текущее положение: x - ' + str(self.x) + ' y - ' + str(self.y))
 
     def change_of_direction(self, direction):
         self.direction = direction
-        print('Текущее направление: ' + direction)
+        print('Текущее направление: ' + direction.upper())
 
 class Bus(Auto):
 
@@ -45,33 +45,40 @@ class Bus(Auto):
         print('Число пассажиров: ' + str(self.number_of_passengers))
 
     def move(self, go):
-        if self.direction == 'Вправо':
+        if self.direction.lower() == 'вправо':
             self.x += abs(go)
-            self.money = go * self.number_of_passengers
+            self.money += go * self.number_of_passengers
             print('Двигаемся вправо на ' + str(go) + ' единиц')
             print('Текущее положение: x - ' + str(self.x) + ' y - ' + str(self.y))
             print('Количество денег: ' + str(self.money))
-        elif self.direction == 'Влево':
+        elif self.direction.lower() == 'влево':
             self.x -= abs(go)
-            self.money = go * self.number_of_passengers
+            self.money += go * self.number_of_passengers
             print('Двигаемся влево на ' + str(go) + ' единиц')
             print('Текущее положение: x - ' + str(self.x) + ' y - ' + str(self.y))
             print('Количество денег: ' + str(self.money))
-        elif self.direction == 'Вверх':
+        elif self.direction.lower() == 'вверх':
             self.y += abs(go)
-            self.money = go * self.number_of_passengers
+            self.money += go * self.number_of_passengers
             print('Двигаемся вверх на ' + str(go) + ' единиц')
             print('Текущее положение: x - ' + str(self.x) + ' y - ' + str(self.y))
             print('Количество денег: ' + str(self.money))
-        elif self.direction == 'Вниз':
+        elif self.direction.lower() == 'вниз':
             self.x -= abs(go)
-            self.money = go * self.number_of_passengers
+            self.money += go * self.number_of_passengers
             print('Двигаемся вниз на ' + str(go) + ' единиц')
             print('Текущее положение: x - ' + str(self.x) + ' y - ' + str(self.y))
             print('Количество денег: ' + str(self.money))
 
-bus = Bus(10, 10, 'Вправо', 10)
+bus = Bus(10, 10, 'вправо', 10)
 bus.enter(15)
 bus.exit(10)
 bus.move(20)
+bus.change_of_direction('Вниз')
+bus.move(30)
+auto = Auto(10, 30, 'Влево')
+auto.move(45)
+auto.change_of_direction('вверх')
+auto.move(50)
+
 
