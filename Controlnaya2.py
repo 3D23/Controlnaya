a00b4e7120847b90542e2342734c44a5ae6477a5
Dima Dmitrichenkov <114085868+3D23@users.noreply.github.com>
2022-12-22 09:39:51 +0300

    Create Controlnaya2.py

---
+++ Controlnaya2.py
@@ -0,0 +1,19 @@
+class Robot():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def move(self, s):
+        for i in s:
+            if i.lower() == 'n' and self.y != 100:
+                self.y += 1
+            elif i.lower() == 's' and self.y != 0:
+                self.y -= 1
+            elif i.lower() == 'e' and self.x != 100:
+                self.x += 1
+            elif i.lower() == 'w' and self.y != 0:
+                self.x -= 1
+        return [self.x, self.y]
+
+robot = Robot(23, 76)
+print(robot.move('sssssweeeeeeewwwwwwNNNEEEEEEEEwwwwww'))
